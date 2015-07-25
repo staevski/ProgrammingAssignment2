4494c2234c2dd3bb7ca4282c0dfd4dbca420270e
Stefan Staevski <stefan@staevski.net>
2015-07-25 12:21:04 +0200

    Programming Assignment 2: Lexical Scoping
    R-Programming - A pair of functions that illustrate how to cache the
    inverse of a matrix to speed up the computation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that illustrate how to cache the inverse of a matrix
+## to speed up the computation
+## Example use and output:
+## > mym <- matrix(c(2,1,5,3), nrow = 2, ncol = 2)
+## > source("cachematrix.R")
+## > myf <- makeCacheMatrix(mym)
+## > cacheSolve(myf)
+##      [,1] [,2]
+## [1,]    3   -5
+## [2,]   -1    2
+## > cacheSolve(myf)
+## Just got the cashed inverted Matrix
+##      [,1] [,2]
+## [1,]    3   -5
+## [2,]   -1    2
+## > 
 
-## Write a short comment describing this function
+## Creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		
+		im <- NULL
+		
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        
+        ##get the initial matrix (data)
+        get <- function() x
+        
+        ##computing the inverse matrix and storing the result into the variable im 
+        setInverse <- function(data) im <<- solve(data)
+        
+        ##just getting the m variable (the inverse matrix) from the environment
+        getInverse <- function() im
+        
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the special matrix returned by "makeCacheMatrix"
+## above. If the inverse has already been calculated 
+##, then the "cacheSolve" function will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        cashedim <- x$getInverse()
+        
+        if(!is.null(cashedim)) {
+                message("Just got the cashed inverted Matrix")
+                return(cashedim)
+        }
+        
+        ##in case there is no cashed inverted matrix, compute it!
+        data <- x$get()
+        im <- x$setInverse(data)
+        im
 }
